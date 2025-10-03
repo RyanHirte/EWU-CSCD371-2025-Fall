c2c465a8b4a76252104ea23694fcc3bd43948290
Ryan Hirte <rhirte@ewu.edu>
2025-10-03 13:12:35 -0700

    FileLogger writes to files
    FileLogger now creates a log file if the file doesn't already exist, and appends to a file if it exists.

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -1,9 +1,74 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
 
 namespace Logger.Tests;
 
 [TestClass]
 public class FileLoggerTests
 {
+    [TestMethod]
+    public void FileLogger_Initializes_Success()
+    {
+        // arrange
+        string testFilePath = "testlog.txt";
+        string LoggerClassName = "TestFileLogger";
 
+        // act
+        FileLogger fileLogger = new FileLogger(testFilePath) { LoggerClassName = LoggerClassName };
+
+        // assert
+        Assert.AreEqual(testFilePath, fileLogger.FilePath);
+        Assert.AreEqual(LoggerClassName, fileLogger.LoggerClassName);
+    }
+
+    [TestMethod]
+    public void Log_CreatesFileIfNotExisting_Success()
+    {
+        // arrange
+        string testFilePath = "./testlog.txt";
+        string LoggerClassName = "TestFileLogger";
+        string testMessage = "This is a test log message.";
+        if (File.Exists(testFilePath))
+        {
+            File.Delete(testFilePath);
+        }
+        FileLogger fileLogger = new FileLogger(testFilePath) { LoggerClassName = LoggerClassName };
+        // act
+        fileLogger.Log(LogLevel.Information, testMessage);
+        // assert
+        Assert.IsTrue(File.Exists(testFilePath), "File was not created by FileLogger");
+        // cleanup
+        if (File.Exists(testFilePath))
+        {
+            File.Delete(testFilePath);
+        }
+    }
+
+    [TestMethod]
+    public void Log_AppendsMessageToFile_Success()
+    {
+        // arrange
+        string testFilePath = "./testlog.txt";
+        string LoggerClassName = "TestFileLogger";
+        string testMessage1 = "This is the first test log message.";
+        string testMessage2 = "This is the second test log message.";
+        if (File.Exists(testFilePath))
+        {
+            File.Delete(testFilePath);
+        }
+        FileLogger fileLogger = new FileLogger(testFilePath) { LoggerClassName = LoggerClassName };
+        // act
+        fileLogger.Log(LogLevel.Information, testMessage1);
+        fileLogger.Log(LogLevel.Information, testMessage2);
+        // assert
+        string[] logContents = File.ReadAllLines(testFilePath);
+        Assert.AreEqual(2, logContents.Length, "Log file does not contain expected number of entries");
+        Assert.AreEqual(testMessage1, logContents[0], "First log message does not match");
+        Assert.AreEqual(testMessage2, logContents[1], "Second log message does not match");
+        // cleanup
+        if (File.Exists(testFilePath))
+        {
+            File.Delete(testFilePath);
+        }
+    }
 }
--- Logger/FileLogger.cs
+++ Logger/FileLogger.cs
@@ -1,8 +1,9 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.IO;
 
 namespace Logger
 {
-    internal class FileLogger : BaseLogger
+    public class FileLogger : BaseLogger
     {
         public string FilePath { get; set; }
         public FileLogger(string filePath)
@@ -11,7 +12,21 @@ namespace Logger
         }
         public override void Log(LogLevel logLevel, string message)
         {
-            // Implementation for logging to a file
+            // If the file does not exist, create it.
+            if (!File.Exists(FilePath))
+            {
+                using (StreamWriter sw = File.CreateText(FilePath))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(FilePath))
+                {
+                    sw.WriteLine(message);
+                }
+            }
         }
     }
 }
