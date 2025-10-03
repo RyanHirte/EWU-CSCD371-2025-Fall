69ff269d24dfcfdde62c71a9eca25c6346626c0c
Ryan Hirte <rhirte@ewu.edu>
2025-10-02 19:20:49 -0700

    First test pass
    Updated csproj files and created unit test and code for logger class name in baselogger.

--- Logger/BaseLogger.cs
+++ Logger/BaseLogger.cs
@@ -2,6 +2,7 @@
 
 public abstract class BaseLogger
 {
+    public required string LoggerClassName { get; set; }
     public abstract void Log(LogLevel logLevel, string message);
 }
 
---
+++ Logger/FileLogger.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Logger
+{
+    internal class FileLogger : BaseLogger
+    {
+        public string FilePath { get; set; }
+        public FileLogger(string filePath)
+        {
+            FilePath = filePath;
+        }
+        public override void Log(LogLevel logLevel, string message)
+        {
+            // Implementation for logging to a file
+        }
+    }
+}
