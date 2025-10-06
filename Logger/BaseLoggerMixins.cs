39346a81fb4f6ea00af843d607ea8d6e3113438c
kellanburns <129882265+kellanburns@users.noreply.github.com>
2025-10-06 13:59:43 -0700

    Cleaned up the .csproj files and flushed out the BaseLoggerMixins.cs file

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -1,6 +1,35 @@
-﻿namespace Logger;
+﻿using System;
+
+namespace Logger;
 
 public static class BaseLoggerMixins
 {
+    public static void Error(this BaseLogger logger, string message)
+    {
+        if (logger == null)
+            throw new ArgumentNullException(nameof(logger));
 
+        logger.Log(LogLevel.Error, message);
+    }
+    public static void Warning(this BaseLogger logger, string message)
+    {
+        if (logger == null)
+            throw new ArgumentNullException(nameof(logger));
+
+        logger.Log(LogLevel.Warning, message);
+    }
+    public static void Information(this BaseLogger logger, string message)
+    {
+        if (logger == null)
+            throw new ArgumentNullException(nameof(logger));
+
+        logger.Log(LogLevel.Information, message);
+    }
+    public static void Debug(this BaseLogger logger, string message)
+    {
+        if (logger == null)
+            throw new ArgumentNullException(nameof(logger));
+
+        logger.Log(LogLevel.Debug, message);
+    }
 }
