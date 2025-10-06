d28d8bcd39afa0fa5c6b016000217f37528278e5
kellanburns <129882265+kellanburns@users.noreply.github.com>
2025-10-06 15:49:24 -0700

    Added params arrays to methods in BaseLoggerMixins.cs

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -4,27 +4,39 @@ namespace Logger;
 
 public static class BaseLoggerMixins
 {
-    public static void Error(this BaseLogger logger, string message)
+    public static void Error(this BaseLogger logger, string message, params object[] args)
     {
         ArgumentNullException.ThrowIfNull(logger);
+
+        if (args.Length > 0)
+            message = string.Format(message, args);
 
         logger.Log(LogLevel.Error, message);
     }
-    public static void Warning(this BaseLogger logger, string message)
+    public static void Warning(this BaseLogger logger, string message, params object[] args)
     {
         ArgumentNullException.ThrowIfNull(logger);
+
+        if (args.Length > 0)
+            message = string.Format(message, args);
 
         logger.Log(LogLevel.Warning, message);
     }
-    public static void Information(this BaseLogger logger, string message)
+    public static void Information(this BaseLogger logger, string message, params object[] args)
     {
         ArgumentNullException.ThrowIfNull(logger);
 
+        if (args.Length > 0)
+            message = string.Format(message, args);
+
         logger.Log(LogLevel.Information, message);
     }
-    public static void Debug(this BaseLogger logger, string message)
+    public static void Debug(this BaseLogger logger, string message, params object[] args)
     {
         ArgumentNullException.ThrowIfNull(logger);
+
+        if (args.Length > 0)
+            message = string.Format(message, args);
 
         logger.Log(LogLevel.Debug, message);
     }
