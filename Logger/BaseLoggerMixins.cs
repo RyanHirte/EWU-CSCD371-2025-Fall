e06fb9dfed21091ae0e6569e3a1300764ee0008b
kellanburns <129882265+kellanburns@users.noreply.github.com>
2025-10-06 14:04:00 -0700

    Fixed exception handling in BaseLoggerMixins.cs

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -6,29 +6,25 @@ public static class BaseLoggerMixins
 {
     public static void Error(this BaseLogger logger, string message)
     {
-        if (logger == null)
-            throw new ArgumentNullException(nameof(logger));
+        ArgumentNullException.ThrowIfNull(logger);
 
         logger.Log(LogLevel.Error, message);
     }
     public static void Warning(this BaseLogger logger, string message)
     {
-        if (logger == null)
-            throw new ArgumentNullException(nameof(logger));
+        ArgumentNullException.ThrowIfNull(logger);
 
         logger.Log(LogLevel.Warning, message);
     }
     public static void Information(this BaseLogger logger, string message)
     {
-        if (logger == null)
-            throw new ArgumentNullException(nameof(logger));
+        ArgumentNullException.ThrowIfNull(logger);
 
         logger.Log(LogLevel.Information, message);
     }
     public static void Debug(this BaseLogger logger, string message)
     {
-        if (logger == null)
-            throw new ArgumentNullException(nameof(logger));
+        ArgumentNullException.ThrowIfNull(logger);
 
         logger.Log(LogLevel.Debug, message);
     }
