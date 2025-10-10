6b849fd8951ba3f60b12764eb91e84ad7a7e3052
kellanburns <129882265+kellanburns@users.noreply.github.com>
2025-10-09 21:24:41 -0700

    Updated filePath manipulation and log testing

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -66,17 +66,28 @@ public class FileLoggerTests
         DateTime now = DateTime.Now;
         string testMessage1 = "This is the first test log message.";
         string testMessage2 = "This is the second test log message.";
-        string expectedLogMessage1 = $"[{now:yyyy-MM-dd HH:mm:ss}] [{LoggerClassName}] [Information] {testMessage1}";
-        string expectedLogMessage2 = $"[{now:yyyy-MM-dd HH:mm:ss}] [{LoggerClassName}] [Information] {testMessage2}";
+        string expectedLogMessage1 = $"[{LoggerClassName}] [Information] {testMessage1}";
+        string expectedLogMessage2 = $"[{LoggerClassName}] [Information] {testMessage2}";
         FileLogger fileLogger = CreateFileLogger();
         // act
         fileLogger.Log(LogLevel.Information, testMessage1);
         fileLogger.Log(LogLevel.Information, testMessage2);
         // assert
         string[] logContents = File.ReadAllLines(TestFilePath);
+
+        int idx0 = logContents[0].IndexOf(']');
+        string log_0 = idx0 >= 0 && idx0 + 2 <= logContents[0].Length
+            ? logContents[0].Substring(idx0 + 2)
+            : logContents[0];
+
+        int idx1 = logContents[1].IndexOf(']');
+        string log_1 = idx1 >= 0 && idx1 + 2 <= logContents[1].Length
+            ? logContents[1].Substring(idx1 + 2)
+            : logContents[1];
+
         Assert.AreEqual(2, logContents.Length, "Log file does not contain expected number of entries");
-        Assert.AreEqual(expectedLogMessage1, logContents[0], "First log message does not match");
-        Assert.AreEqual(expectedLogMessage2, logContents[1], "Second log message does not match");
+        Assert.AreEqual(expectedLogMessage1, log_0, "First log message does not match");
+        Assert.AreEqual(expectedLogMessage2, log_1, "Second log message does not match");
     }
 
     [TestMethod]
@@ -85,12 +96,18 @@ public class FileLoggerTests
         // arrange
         DateTime now = DateTime.Now;
         string testMessage = "This is a test log message.";
-        string expectedLogMessage = $"[{now:yyyy-MM-dd HH:mm:ss}] [{LoggerClassName}] [Information] {testMessage}";
+        string expectedLogMessage = $"[{LoggerClassName}] [Information] {testMessage}";
         FileLogger fileLogger = CreateFileLogger();
         // act
         fileLogger.Log(LogLevel.Information, testMessage);
         // assert
         string[] logContents = File.ReadAllLines(TestFilePath);
-        Assert.AreEqual(expectedLogMessage, logContents[0]);
+
+        int idx = logContents[0].IndexOf(']');
+        string log_0 = idx >= 0 && idx + 2 <= logContents[0].Length
+            ? logContents[0].Substring(idx + 2)
+            : logContents[0];
+
+        Assert.AreEqual(expectedLogMessage, log_0);
     }
 }
