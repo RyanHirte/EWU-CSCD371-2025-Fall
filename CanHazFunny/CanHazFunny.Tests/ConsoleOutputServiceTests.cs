4b02e2af0fb02e0e1f4d0d7ee49bbfa18cf10457
Ryan Hirte <rhirte@ewu.edu>
2025-10-10 07:43:26 -0700

    Extra credit unit tests
    Attempt at extra credit unit tests.

---
+++ CanHazFunny/CanHazFunny.Tests/ConsoleOutputServiceTests.cs
@@ -0,0 +1,60 @@
+using System;
+using System.IO;
+using Xunit;
+
+namespace CanHazFunny.Tests
+{
+    public class ConsoleOutputServiceTests
+    {
+        [Fact]
+        public void WriteJoke_WithMessage_WritesToConsole()
+        {
+            // Arrange
+            var service = new ConsoleOutputService();
+            var joke = "Why did the programmer quit his job? Because he didn't get arrays.";
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            try
+            {
+                // Act
+                service.WriteJoke(joke);
+
+                // Assert
+                Assert.Equal(joke + Environment.NewLine, stringWriter.ToString());
+            }
+            finally
+            {
+                // Cleanup - restore console output
+                var standardOutput = new StreamWriter(Console.OpenStandardOutput());
+                standardOutput.AutoFlush = true;
+                Console.SetOut(standardOutput);
+            }
+        }
+
+        [Fact]
+        public void WriteJoke_WithEmptyString_WritesEmptyLine()
+        {
+            // Arrange
+            var service = new ConsoleOutputService();
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            try
+            {
+                // Act
+                service.WriteJoke(string.Empty);
+
+                // Assert
+                Assert.Equal(Environment.NewLine, stringWriter.ToString());
+            }
+            finally
+            {
+                // Cleanup - restore console output
+                var standardOutput = new StreamWriter(Console.OpenStandardOutput());
+                standardOutput.AutoFlush = true;
+                Console.SetOut(standardOutput);
+            }
+        }
+    }
+}
