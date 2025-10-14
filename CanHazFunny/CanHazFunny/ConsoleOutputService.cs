b1a3b2ce28bac6fe4a0a9227fe999a1bd2a9fdd5
Joseph Potapenko <129787506+JosephPotapenko@users.noreply.github.com>
2025-10-13 23:48:45 -0700

    Checking for null outputs

--- CanHazFunny/CanHazFunny/ConsoleOutputService.cs
+++ CanHazFunny/CanHazFunny/ConsoleOutputService.cs
@@ -6,6 +6,8 @@ public class ConsoleOutputService : IOutputService
 {
     public void WriteJoke(string joke)
     {
+        if (joke is null) throw new ArgumentNullException(nameof(joke));
+
         Console.WriteLine(joke);
     }
 }
