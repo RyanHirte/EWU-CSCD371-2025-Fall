8972678de893e7495bdc09bc9fda56ccae71b8e3
Joseph Potapenko <129787506+JosephPotapenko@users.noreply.github.com>
2025-10-13 23:51:55 -0700

    Nevermind

--- CanHazFunny/CanHazFunny/ConsoleOutputService.cs
+++ CanHazFunny/CanHazFunny/ConsoleOutputService.cs
@@ -6,8 +6,6 @@ public class ConsoleOutputService : IOutputService
 {
     public void WriteJoke(string joke)
     {
-        if (joke is null) throw new ArgumentNullException(nameof(joke));
-
         Console.WriteLine(joke);
     }
 }
