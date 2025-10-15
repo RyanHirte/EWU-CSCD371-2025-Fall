ef88da2fb6b43ebb2ac047c75cdc7e609084f07d
Ryan <ryan.hirte21@gmail.com>
2025-10-15 10:12:50 -0700

    Disposed properly of json object
    used a using statement to dispose of json object in jokeservice.

--- CanHazFunny/CanHazFunny/JokeService.cs
+++ CanHazFunny/CanHazFunny/JokeService.cs
@@ -11,8 +11,10 @@ public class JokeService : IJokeService
     {
         // Extra credit: API gets joke in json format
         var apiResponse = HttpClient.GetStringAsync("https://geek-jokes.sameerkumar.website/api?format=json").Result;
-        var jokeObject = JsonDocument.Parse(apiResponse);
-        string joke = jokeObject.RootElement.GetProperty("joke").GetString() ?? "No joke received from api.";
-        return joke;
+        using (var jokeObject = JsonDocument.Parse(apiResponse))
+        {
+            string joke = jokeObject.RootElement.GetProperty("joke").GetString() ?? "No joke received from api.";
+            return joke;
+        }
     }
 }
