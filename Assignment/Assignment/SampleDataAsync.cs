0cb974a89e82674fe641ee20b5c51e39f2f6b99f
Ryan Hirte <rhirte@ewu.edu>
2025-11-15 13:32:03 -0800

    SampleDataAsync Created

---
+++ Assignment/Assignment/SampleDataAsync.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment;
+
+public class SampleDataAsync : IAsyncSampleData
+{
+    public IAsyncEnumerable<string> CsvRows => throw new NotImplementedException();
+
+    public IAsyncEnumerable<IPerson> People => throw new NotImplementedException();
+
+    public IAsyncEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(Predicate<string> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetAggregateListOfStatesGivenPeopleCollection(IAsyncEnumerable<IPerson> people)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetAggregateSortedListOfStatesUsingCsvRows()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IAsyncEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows()
+    {
+        throw new NotImplementedException();
+    }
+}
